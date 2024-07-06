034dd9c521d2f45ea7a8ccdc1e691422f3d9306c
RodrigoGottschall <ro_criscuolo@hotmail.com>
2024-07-06 14:17:02 -0300

    Notifications bell

--- mercadinho-do-ze/components/HomeScreen/Header.tsx
+++ mercadinho-do-ze/components/HomeScreen/Header.tsx
@@ -1,21 +1,43 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Modal } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import WineTextContainer from "./WineTextContainer";
-import {
-  useFonts,
-  Nunito_400Regular,
-  Nunito_700Bold,
-} from "@expo-google-fonts/nunito";
 
 const HeaderHome: React.FC = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleBellPress = () => {
+    setModalVisible(true);
+  };
+
   return (
     <>
       <View style={styles.containerOuter}>
-        <Icon name="bell" size={12} color="#F7A833" />
+        <TouchableOpacity onPress={handleBellPress}>
+          <Icon name="bell" size={12} color="#F7A833" />
+        </TouchableOpacity>
         <Text style={styles.title}>Mercadinho do Zé</Text>
         <Icon name="search" size={18} color="#F7A833" />
       </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>Não há notificações</Text>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <Text style={styles.closeButton}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
       <WineTextContainer />
     </>
   );
@@ -35,6 +57,25 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#323232",
     fontFamily: "Nunito_700Bold",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Fundo transparente
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  closeButton: {
+    color: "#F7A833", // Use a cor do seu tema
   },
 });
 
